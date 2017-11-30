110df2e543e54e519e7f9b8ad384abca548eefa2
teilon <teiloncw@gmail.com>
2017-11-30 18:48:52 +0600

    fixed allowed_hosts

--- app_fhet/settings.py
+++ app_fhet/settings.py
@@ -28,7 +28,7 @@ DEBUG = False
 
 #ALLOWED_HOSTS = []
 #ALLOWED_HOSTS = ['127.0.0.1', 'fhet.pythonanywhere.com']
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'fhetblog.herokuapp.com']
 
 
 
